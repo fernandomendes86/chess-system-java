61f495e0ff3e73c0b14bc2cb55bf25e0c2de3f23
Fernando Mendes <fernando.msantos1@gmail.com>
2020-03-13 09:44:58 -0300

    update class Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -10,7 +10,26 @@ public class Board {
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
+		
 	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	
 	
 	
 }
